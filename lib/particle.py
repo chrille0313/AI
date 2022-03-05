531b56fd59f8e72a4ff792ed72eed40b9cf95411
Christian <chrille_0313@hotmail.com>
2022-03-05 20:18:58 +0100

    Refactor

--- lib/particle.py
+++ lib/particle.py
@@ -2,18 +2,23 @@ from pygame import Vector2
 from draw import draw_circle
 
 
+GRAVITY = Vector2(0, 0.001)
+
+
 class Particle:
-    def __init__(self, x, y, charge=1, mass=10, velX=0, velY=0, accX=0, accY=0):
+    def __init__(self, x, y, charge=1, mass=10, velX=0, velY=0, accX=0, accY=0, shouldSimulate=True):
         self.pos = Vector2(x, y)
         self.velocity = Vector2(velX, velY)
         self.acceleration = Vector2(accX, accY)
         self.charge = charge
         self.mass = mass
+        self.shouldSimulate = shouldSimulate
         self.color = (0, 0, max(255, charge)) if charge <= 0 else (max(255, -charge), 0, 0)
 
     def draw(self):
         draw_circle(self.pos, self.mass, self.color)
 
     def simulate(self):
-        self.velocity += self.acceleration
+        self.velocity += self.acceleration + GRAVITY * self.shouldSimulate
         self.pos += self.velocity
+
--- main.py
+++ main.py
@@ -2,29 +2,31 @@ from lib.func import *
 
 
 class App:
-    def __init__(self, windowW, windowH):
+    def __init__(self, windowW, windowH, fps):
         pygame.init()
 
         self.windowSize = self.windowW, self.windowH = windowW, windowH
         self.window = pygame.display.set_mode(self.windowSize)
+        self.clock = pygame.time.Clock()
+        self.fps = fps
+        self.running = True
 
         self.vectorField = VectorField(windowW, windowH, 20, Particle)
 
         self.particles = []
 
-        for i in range(4):
-            self.particles.append(
-                Particle(random.randint(0, windowW), random.randint(0, windowH), 10 if i % 2 != 0 else -10))
+        self.particles.append(Particle(windowW/2, windowH/2, 10))
+        self.particles.append(Particle(windowW / 2, windowH, 10, shouldSimulate=False))
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                self.running = False
 
     def draw(self):
         self.window.fill(Colors.black)
 
-        self.vectorField.draw()
+        # self.vectorField.draw()
 
         for particle in self.particles:
             particle.draw()
@@ -32,24 +34,27 @@ class App:
         pygame.display.update()
 
     def main_loop(self):
-        while True:
+        while self.running:
             self.events()
 
             for particle1 in self.particles:
                 for particle2 in self.particles:
                     if particle1 != particle2:
-                        particle1.velocity += affect(particle1, particle2)
-                        particle2.velocity += affect(particle2, particle1)
+                        if particle1.shouldSimulate:
+                            particle1.velocity += affect(particle1, particle2)
+                        if particle2.shouldSimulate:
+                            particle2.velocity += affect(particle2, particle1)
 
             for particle in self.particles:
                 particle.simulate()
 
-            for vector in self.vectorField.vectors:
-                vector.update(self.particles, Particle, affect)
+            # for vector in self.vectorField.vectors:
+                # vector.update(self.particles, Particle, affect)
 
             self.draw()
+            self.clock.tick(self.fps)
 
 
 if __name__ == '__main__':
-    app = App(1280, 720)
+    app = App(1280, 720, 0)
     app.main_loop()
