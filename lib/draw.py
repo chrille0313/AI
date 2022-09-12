3aadc3b77e2a57d41afe448786d9950425588eb8
Christian <chrille_0313@hotmail.com>
2022-09-12 12:02:56 +0200

    Update draw.py

--- lib/draw.py
+++ lib/draw.py
@@ -11,6 +11,10 @@ def draw_line(start, end, color=Colors.white):
     pygame.draw.line(pygame.display.get_surface(), color, start, end)
 
 
+def draw_rect(pos, size, color=Colors.white, hollow=False):
+    pygame.draw.rect(pygame.display.get_surface(), color, (*pos, *size), hollow)
+
+
 def draw_arrow(start, end, trirad=1, lcolor=Colors.white, tricolor=Colors.white):
     pygame.draw.line(pygame.display.get_surface(), lcolor, start, end)
     rotation = degrees(arctan2(start[1] - end[1], end[0] - start[0])) + 90
