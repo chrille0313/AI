d542f775ede48ff0b7509d484a765f744f108b51
Christian <chrille_0313@hotmail.com>
2023-10-21 17:24:35 +0200

    Move files

---
+++ lib/GUI/gui.py
@@ -0,0 +1,24 @@
+
+class GUI:
+	def __init__(self, windowSize):
+		pass
+
+	def update(self):
+		pass
+
+	def events(self):
+		pass
+
+	def draw(self):
+		pass
+
+	def quit(self):
+		pass
+
+"""
+class PygameGUI(GUI):
+	def __init__(self, windowSize):
+		super().__init__(windowSize)
+		pygame.init()
+		self.window = pygame.display.set_mode(windowSize)
+"""
